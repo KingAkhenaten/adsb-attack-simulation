ccc526c81a788989ecd2b673a6642c8fae5583b3
Christian Clay <clayck@etsu.edu>
2022-10-30 23:20:36 -0400

    changed name of file

---
+++ sel_jam_attack.py
@@ -0,0 +1,51 @@
+# Simulates ADS-B DoS attack
+# Run flight sim with regular data set
+# Then run this program with a set target airplane
+# Rerun flight sim with edited csv data
+import csv
+import argparse
+
+def main():
+	filename = "testDoS.csv"
+	rows = []
+	field_iter = 0
+	# target = "DLH2WA"
+
+	parser = argparse.ArgumentParser(description="Simulates ADS-B deviation attack")
+	parser.add_argument("target",
+					help="Specify a target to attack")
+	parser.add_argument("filename",
+					help="Specify the file to work with")
+	parser.add_argument("-v", "--verbose", dest="verbose",
+                    help="Output verbose logging",
+                    action="store_true")
+
+	args = parser.parse_args()
+
+	if args.verbose:
+		print("[*] Target is %s" % args.target)
+
+	with open(filename, 'r') as csvfile:
+		csvreader = csv.reader(csvfile)
+
+		# assume no header
+		for row in csvreader:
+			rows.append(row)
+
+	# column 9 is cs_icao (ICAO call sign)
+	for col in rows[:csvreader.line_num]:
+		if col[9] == args.target:
+			field_iter += 1
+			rows.remove(col)
+	if args.verbose:
+		print("[*] Two entries for %s found" % args.target)
+	field_iter = 0
+
+	# check array after removal
+	for col in rows[:csvreader.line_num]:
+		if col[9] == args.target:
+			field_iter += 1
+
+	if field_iter == 0:
+		print("[%s] DoS completed" % args.target)
+main()
